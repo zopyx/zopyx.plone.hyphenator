8e2950bfb2c065ef0b1951062dbdd85daa1f7a4a
Andreas JungÂ <info@zopyx.com>
2016-07-27 21:13:32 +0200

    updated

--- zopyx/plone/hyphenator/browser/resources/js-compiled-min.js
+++ zopyx/plone/hyphenator/browser/resources/js-compiled-min.js
@@ -5,7 +5,7 @@
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
-/* Last merge : Wed Jul 27 20:39:12 CEST 2016  */
+/* Last merge : Wed Jul 27 21:13:31 CEST 2016  */
 
 /* Merging order :
 
